67075e8b58e1b09c8be40c0f0e784e9a44c9819c
raj sang <rajsang@rajs-MacBook-Air.local>
2025-04-03 12:26:10 +0530

    Docker yaml file push

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("app.py")
+from flask import Flask
+print("hello")
+
+app=Flask(__name__)
+
+@app.route("/")
+
+def home():
+    return "Hello World!"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=8080)
